62d1b6b2f48876835b5f139a1f22a65c88674952
Craffic <392301161@qq.com>
2020-07-04 16:50:32 +0800

    Spring-Security-oauth2 基于内存存储令牌 dml语句

---
+++ hello-spring-security-oauth2-server/src/main/resources/sql/dml.sql
@@ -0,0 +1,2 @@
+insert into clientdetails (APPID, RESOURCEIDS, APPSECRET, SCOPE, GRANTTYPES, REDIRECTURL, AUTHORITIES, ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY, ADDITIONALINFORMATION, AUTOAPPROVESCOPES)
+values ('client', null, '$2a$10$d2bU.iXsf2wX5ewZ6c5zfOTlecHe9XdSuCYdwSe3tKUKrHnzibiNO', 'app', 'authorization_code', 'http://www.funtl.com', null, null, null, null, null);
